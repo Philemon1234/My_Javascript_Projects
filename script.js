bfcfb1b6057c4666bc829f7f88644c825ad4e128
Philemon1234 <ntivuguruzwaphilemon0@gmai.com>
2023-03-15 00:55:19 +0300

    Updated files

---
+++ script.js
@@ -0,0 +1,6 @@
+// const bannerContent = document.querySelector(".banner-content");
+
+// window.addEventListener("load", function(){
+//     bannerContent.style.transform = "translateX(20%)";
+//     // alert("Load");
+// })
