28e4ab9a8b43e1c148e74809489cf4729c48a128
Philemon1234 <ntivuguruzwaphilemon0@gmai.com>
2023-03-27 09:40:19 +0300

    Updated files

---
+++ xcorebit Project/script.js
@@ -0,0 +1,26 @@
+// const bannerContent = document.querySelector(".banner-content");
+
+// window.addEventListener("load", function(){
+//     bannerContent.style.transform = "translateX(20%)";
+//     // alert("Load");
+// })
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// });
