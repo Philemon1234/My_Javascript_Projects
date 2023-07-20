7c95d5ac09d5395c2def71e2c05cc849211720e0
NTIHINDUKA ALPHA <ntihindukaalpha@yahoo.com>
2023-07-20 05:42:49 +0300

    Added chabges to all files

---
+++ Passenger Counter App/index.js
@@ -0,0 +1,14 @@
+// document.getElementById("count-el").innerText = 5;
+
+//intialize the count as 0
+//Listen for clicks on the increment button
+//increment the count variable when the button is clicked
+//change the count-el in the HTML to reflect the new count
+
+let count = 0;
+let countEl = document.getElementById("count-el");
+function increment() {
+    countEl.innerText = count;
+    count = count + 1;
+}
+
