c70dae60cf26ba39d8156f4e6e0793123ad3c1d1
NTIHINDUKA ALPHA <ntihindukaalpha@yahoo.com>
2023-08-11 08:50:14 +0300

    Completed rate app

--- Interactive-rating-component/app.js
+++ Interactive-rating-component/app.js
@@ -2,18 +2,26 @@ let roundBtns = document.querySelectorAll("#round-btn");
 let submitBtn = document.querySelector("#submit");
 let rateNumber = document.querySelector("#rate-number");
 let thankCard = document.querySelector("#thank_card");
-
+let selectedRate = null;
 
 
 for (let i = 0; i < roundBtns.length; i++) {
-    roundBtns[i].addEventListener("click", function(event) {
+    roundBtns[i].addEventListener("click", function (event) {
         let clicked = event.target.textContent;
         console.log(clicked);
-        
+
+        selectedRate = clicked;
+
         rateNumber.textContent = clicked;
+
     })
 }
 
-submitBtn.addEventListener("click", function() {
-    thankCard.classList.toggle("hide_card");
+submitBtn.addEventListener("click", function () {
+    if (selectedRate) {
+        thankCard.classList.toggle("hide_card");
+    } else {
+        alert("Please select a rate before submitting.")
+    }
 })
+
