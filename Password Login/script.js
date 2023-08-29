0cd3a0d61f3b96552a8cec9eb9af5cd9084b91ad
NTIHINDUKA ALPHA <ntihindukaalpha@yahoo.com>
2023-08-29 12:20:22 +0300

    updated files

---
+++ Password Login/script.js
@@ -0,0 +1,48 @@
+let input = document.getElementById("input-form")
+let submitBtn = document.getElementById("submit-btn")
+let parent = document.getElementById("parent")
+let timeContainer = document.querySelector(".time-container")
+let formContainer = document.getElementById("form-container")
+let incorrectPasswordContainer = document.getElementById("incorrect-password-container")
+// let icons = document.querySelector(".icons")
+
+submitBtn.addEventListener("click", function () {
+    getPassword()
+})
+
+parent.addEventListener("click", hideTimeContainer)
+// icons.addEventListener("click", hideTimeContainer)
+
+function hideTimeContainer() {
+    timeContainer.style.top = "-37%"
+    timeContainer.style.transition = "2s"
+    timeContainer.style.opacity = "0"
+
+    formContainer.style.opacity = "1";
+    formContainer.style.transition = ".3s 1s";
+    // formContainer.style.display = "block";
+
+
+    incorrectPasswordContainer.style.opacity = "0";
+    incorrectPasswordContainer.style.display = "none";
+
+
+    // parent.style.filter = "blur(5px)";
+    // parent.style.zIndex = - 2
+}
+
+function getPassword() {
+    let inputValue = input.value;
+
+    if (inputValue === "2006") {
+        alert("Welcome😃")
+    } else {
+
+        formContainer.style.opacity = "0";
+        formContainer.style.display = "none";
+
+
+        incorrectPasswordContainer.style.opacity = "1";
+        // incorrectPasswordContainer.style.display = "block";
+    }
+}
