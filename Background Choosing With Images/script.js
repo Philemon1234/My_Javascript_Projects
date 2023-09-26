a8722da756076570a64bfc7461c6aa144af36ed7
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2023-09-26 08:37:18 +0300

    completed Background Choosing With Images project

--- Background Choosing With Images/script.js
+++ Background Choosing With Images/script.js
@@ -1,32 +1,25 @@
-let body = document.querySelector("body")
-let selectDropdown = document.querySelector("#select")
+let container = document.querySelector(".container");
+let select = document.getElementById("select");
 
-selectDropdown.addEventListener("change", changeImage);
+select.addEventListener("change", changeImage);
 
 function changeImage() {
+    let selected = select.value;
 
-    let selected = selectDropdown.value;
-
-    if (selected === "none") {
-        body.style.backgroundImage = "none";
-    }
-
-    if (selected === "batman") {
-        body.style.backgroundImage = "url('https://wallpapercave.com/dwp1x/vD2vB1p.jpg')"
-    }
-     else if (selected === "superman") {
-        body.style.backgroundImage = "url('https://wallpapercave.com/uwp/uwp3890541.jpeg')"
-    }
-    if (selected === "spiderman") {
-        body.style.backgroundImage = "url('https://wallpapercave.com/dwp1x/wp12363024.png')"
-    }
-    if (selected === "joker") {
-        body.style.backgroundImage = "url('https://wallpapercave.com/uwp/uwp3670974.jpeg')"
-    }
-    if (selected === "captain-america") {
-        body.style.backgroundImage = "url('https://wallpapercave.com/dwp1x/wp4657881.jpg')"
-    }
-    if (selected === "aqua-man") {
-        body.style.backgroundImage = "url('https://wallpapercave.com/dwp1x/wp3869426.jpg')"
+    if (selected === 'none') {
+        container.style.backgroundImage = "none"
+    } else if (selected === 'batman') {
+        container.style.backgroundImage = "url('batman.jpg')";
+    } else if (selected === 'superman') {
+        container.style.backgroundImage = "url('superman.jpg')";
+    } else if (selected === 'spiderman') {
+        container.style.backgroundImage = "url('spiderman.jpg')";
+    } else if (selected === 'joker') {
+        container.style.backgroundImage = "url('joker.jpeg')";
+    } else if (selected === 'captain-america') {
+        container.style.backgroundImage = "url('captain-america.jpg')";
+    } else if (selected === 'aqua-man') {
+        container.style.backgroundImage = "url('aquaman.jpg')";
     }
 }
+
